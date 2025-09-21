2461fc7ea14cef8c06bf8846c96052a1e2d89550
raj63829 <rajsharma63829@gmail.com>
2025-09-21 12:51:51 +0530

    build5

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ✅ required for Netlify root deploy
+  base: "/", // 👈 required for Netlify
 })
