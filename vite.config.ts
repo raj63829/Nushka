c24e38356c59a2b1bfe0ade478746f9827d1043c
raj63829 <rajsharma63829@gmail.com>
2025-10-08 03:22:06 +0530

    vite error

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
-server: {
-  proxy: {
-    '/api': {
-      target: 'http://casper-ai-573fqmg7wa-uc.a.run.app',
-      changeOrigin: true,
-      rewrite: path => path.replace(/^\/api/, ''),
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://casper-ai-573fqmg7wa-uc.a.run.app",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
     },
   },
-}
+
+  // Optional: build options
+  build: {
+    outDir: "dist",
+  },
+})
